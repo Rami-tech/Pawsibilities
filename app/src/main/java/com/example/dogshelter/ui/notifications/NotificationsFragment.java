f88d79c20a9b86895abe156afa7bbdfff04ec6b9
geo <ge.or.gi.o@hotmail.com>
2020-12-05 12:14:35 +0200

    map shows in notifications dashboard, still need to add marker and moveCamera

--- app/src/main/java/com/example/dogshelter/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/dogshelter/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.dogshelter.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,24 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dogshelter.R;
+import com.example.dogshelter.storyActivity;
+import com.example.dogshelter.ui.dashboard.DashboardViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -23,13 +35,23 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng loc1 = new LatLng(33.948738, 35.674530);
+        googleMap.addMarker(new MarkerOptions().position(loc1).title("Headquarter 1"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc1));
+
     }
 }
