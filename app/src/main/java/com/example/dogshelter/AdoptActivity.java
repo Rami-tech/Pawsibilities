efcb4bb4977b93fdf68a6a29b4170f4b1731e037
geo <ge.or.gi.o@hotmail.com>
2020-12-14 23:22:09 +0200

    added firebase connectivity + retrieving map values

--- app/src/main/java/com/example/dogshelter/AdoptActivity.java
+++ app/src/main/java/com/example/dogshelter/AdoptActivity.java
@@ -3,10 +3,21 @@ package com.example.dogshelter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.dogshelter.ui.home.AdoptFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdoptActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://pawsibilities-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,29 @@ public class AdoptActivity extends AppCompatActivity {
                     .replace(R.id.container, AdoptFragment.newInstance())
                     .commitNow();
         }
-//        getSupportActionBar().setTitle("Adopt");
+
+        DatabaseReference myRef = database.getReference("Dogs");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            private final String TAG = MainActivity.class.getSimpleName();
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
     }
 }
